67c42c5b8122375750b406006c53da33bf34ab4a
Kavisha Srivastava <kavishasri07@gmail.com>
2026-09-17 00:10:45 +0530

    Call by Reference and Value

---
+++ callbyreference.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int data(int &x) {
+    x= 50;
+    cout<<x;
+}
+int main() {
+    int m= 100;
+    data(m);
+    cout<<m;
+    return 0;
+}
---
+++ callbyvalue.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int data(int x) {
+    x= 50;
+    cout<<x;
+    return x;
+}
+int main() {
+    int m= 100;
+    data(m);
+    cout<<m;
+    return 0;
+}
