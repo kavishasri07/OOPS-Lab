9a0f32a8f536396ef708d4e9a1e351213618243b
Kavisha Srivastava <kavishasri07@gmail.com>
2026-09-17 00:11:25 +0530

    Default function

---
+++ defaultfnx.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+void data(string name="Student") {
+    cout<<name;
+}
+int main() {
+    data();
+    cout<<endl;
+    data("Hello");
+    return 0;
+}
